66dac118ce6db18c10fb048b4f6bc9e66bf22d26
localmanmokemo-byte <localmanmokemo@gmail.com>
2026-07-02 16:41:44 +0300

    Create webpack.config.js for bundling

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    port: 8080,
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js'],
+  }
+};
